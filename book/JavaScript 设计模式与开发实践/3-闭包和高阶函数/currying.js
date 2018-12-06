97a0755bf5033592fb9938e55704cffe33788819
yinfan <yinfan@iqianjin.com>
2018-12-06 15:17:57 +0800

    新增：函数柯里化（部分求值）

---
+++ book/JavaScript 设计模式与开发实践/3-闭包和高阶函数/currying.js
@@ -0,0 +1,29 @@
+function currying(fn) {
+	let args = []
+
+	return function () {
+		if (arguments.length) {
+			[].push.apply(args, arguments)
+		} else {
+			fn.call(this, args)
+		}
+	}
+}
+
+function cost() {
+	let money = 0;
+	let arg = Array.from(arguments[0]);
+
+	for (let i = 0; i < arg.length; i++) {
+		money += arg[i]
+	}
+
+	console.log(money)
+}
+
+
+let t = currying(cost)
+
+t(10, 1);
+t(22);
+t();
