73b659c42789549222e6a4ba8035f7f322912e46
yinfan <yinfan@iqianjin.com>
2019-03-07 15:08:00 +0800

    新增：测试webpack构建速度

--- 前端/打包工具/webpack_pratice/webpack.config-12.js
+++ 前端/打包工具/webpack_pratice/webpack.config-12.js
@@ -2,9 +2,12 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');  // 自动生成html文件
 const CleanWebpackPlugin = require('clean-webpack-plugin'); // 执行前清空dist文件夹
 const ManifestPlugin = require('webpack-manifest-plugin');  // 模块映射到输出bundle的过程
+const SpeedMeasurePlugin = require("speed-measure-webpack-plugin"); // 测试webpack构建速度
 const webpack = require('webpack');
 
-module.exports = {
+const smp = new SpeedMeasurePlugin();
+
+module.exports = smp.wrap({
 	entry: {
 		app: './src/12-ts/index.ts',
 	},
@@ -33,4 +36,4 @@ module.exports = {
 		new webpack.HotModuleReplacementPlugin()
 	],
 	devtool: 'inline-source-map',
-}
+})
