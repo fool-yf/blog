04e72786be820ce9b7fb8264f6dcc26d24fd4cc7
yinfan <yinfan@iqianjin.com>
2019-03-07 15:04:06 +0800

    修改：ts

---
+++ 前端/打包工具/webpack_pratice/src/12-ts/index.ts
@@ -0,0 +1,5 @@
+function f(name: string): string {
+    return `${name}: 我在`
+}
+
+console.log(f('小爱同学'))
---
+++ 前端/打包工具/webpack_pratice/webpack.config-12.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');  // 自动生成html文件
+const CleanWebpackPlugin = require('clean-webpack-plugin'); // 执行前清空dist文件夹
+const ManifestPlugin = require('webpack-manifest-plugin');  // 模块映射到输出bundle的过程
+const webpack = require('webpack');
+
+module.exports = {
+	entry: {
+		app: './src/12-ts/index.ts',
+	},
+	output: {
+		filename: '[name].bundle.[hash].js',
+		path: path.resolve(__dirname, 'dist12')
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/
+			}
+		]
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js']
+	},
+	plugins: [
+		new CleanWebpackPlugin(['dist12']),
+		new HtmlWebpackPlugin({
+			title: 'HMR'
+		}),
+		new ManifestPlugin(),
+		new webpack.HotModuleReplacementPlugin()
+	],
+	devtool: 'inline-source-map',
+}
