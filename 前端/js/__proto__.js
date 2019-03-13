e8d312cfc74267f9c283c996c7eca6dd165b50ed
yinfan <yinfan@iqianjin.com>
2019-03-13 17:22:52 +0800

    新增：__proto__注释

--- 前端/js/__proto__.js
+++ 前端/js/__proto__.js
@@ -1,3 +1,10 @@
+/**
+ * __proto__： 隐式原型，默认指向该对象的构造函数的原型(prototype)
+ * Object.__prop__ === Function.__proto__ === F.__proto__ === Function.prototype
+ * Object.prototype  ===  F.prototype.__proto__  === Function.prototype.__proto__
+ * Object.prototype.__proto__ === null
+ * @constructor
+ */
 function F() {
 	console.log('F....')
 }
@@ -6,11 +13,11 @@ let f = new F();
 console.log(f.__proto__ === F.prototype); // true
 console.log(F.__proto__ === Function.prototype); // true
 console.log(Function.__proto__ === Function.prototype) // true
+console.log(Object.__proto__ === Function.prototype); // true
 
 console.log(F.__proto__ === Function.__proto__); // true
-
-console.log(Object.__proto__ === Function.prototype); // true
 console.log(Object.__proto__ === Function.__proto__); // true
+console.log(F.__proto__ === Object.__proto__) // true
 
 console.log(F.prototype.__proto__ === Object.prototype) // true
 console.log(Function.prototype.__proto__ === Object.prototype)  // true
