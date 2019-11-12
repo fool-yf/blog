239dba5108f65cd0b1793a0a6d9dbca286c3dfbc
yinfan <tech_yinfan@163.com>
2019-11-12 14:54:26 +0800

    add: 快速排序

---
+++ book/算法图解/4. 快速排序/quickSort.js
@@ -0,0 +1,21 @@
+function quickSort(arr) {
+  if (arr.length < 2) {
+    return arr
+  }
+
+  let base = arr.splice(0, 1);
+  let left = [];
+  let right = [];
+
+  for(let i = 0; i < arr.length; i++) {
+    if (arr[i] < base) {
+      left.push(arr[i])
+    } else {
+      right.push(arr[i])
+    }
+  }
+
+  return quickSort(left).concat(base, quickSort(quickSort(right)))
+}
+
+console.log(quickSort([5,2,1,7,3,4]))
